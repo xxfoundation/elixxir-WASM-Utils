8c6d01e497b1a957876484f8c453b6de6ee17948
thisisommore <ommore501@gmail.com>
2025-03-05 13:49:07 +0000

    Add ExternalStorage implementation

---
+++ storage/externalStorage.go
@@ -0,0 +1,333 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
+
+//go:build js && wasm
+
+package storage
+
+import (
+	"os"
+	"strings"
+	"syscall/js"
+
+	"github.com/Max-Sum/base32768"
+
+	"gitlab.com/elixxir/wasm-utils/exception"
+	"gitlab.com/elixxir/wasm-utils/utils"
+)
+
+// externalStorageWasmPrefix is prefixed to every keyName saved to external storage by
+// externalStorage. It allows the identification and deletion of keys only created
+// by this WASM binary while ignoring keys made by other scripts on the same
+// page.
+//
+// The chosen prefix is two characters, that when converted to UTF16, take up 4
+// bytes without any zeros to make them more unique.
+const externalStorageWasmPrefix = "🞮🞮"
+
+// ExternalStorage defines an interface for setting persistent state in a KV format
+// specifically for web-based implementations.
+type ExternalStorage interface {
+	// Get decodes and returns the value from the external storage given its key
+	// name. Returns os.ErrNotExist if the key does not exist.
+	Get(key string) ([]byte, error)
+
+	// Set encodes the bytes to a string and adds them to external storage at the
+	// given key name. Returns an error if external storage quota has been reached.
+	Set(key string, value []byte) error
+
+	// RemoveItem removes a key's value from external storage given its name. If
+	// there is no item with the given key, this function does nothing.
+	RemoveItem(keyName string) error
+
+	// Clear clears all the keys in storage. Returns the number of keys cleared and any error.
+	Clear() (int, error)
+
+	// ClearPrefix clears all keys with the given prefix. Returns the number of
+	// keys cleared and any error.
+	ClearPrefix(prefix string) (int, error)
+
+	// Key returns the name of the nth key in externalStorage. Returns
+	// os.ErrNotExist if the key does not exist. The order of keys is not
+	// defined.
+	Key(n int) (string, error)
+
+	// Keys returns a list of all key names in external storage.
+	Keys() ([]string, error)
+
+	// Length returns the number of keys in externalStorage.
+	Length() (int, error)
+
+	// ExternalStorageUNSAFE returns the underlying external storage wrapper. This can
+	// be UNSAFE and should only be used if you know what you are doing.
+	//
+	// The returned wrapper wraps all the functions and fields on the Javascript
+	// havenStorage object to handle type conversions and errors. But it does
+	// not decode/sanitize the inputs/outputs or track entries using the prefix
+	// system. If using it, make sure all key names and values can be converted
+	// to valid UCS-2 strings.
+	ExternalStorageUNSAFE() *HavenStorageJS
+}
+
+// externalStorage contains the js.Value representation of havenStorage.
+type externalStorage struct {
+	// The Javascript value containing the havenStorage object
+	v *HavenStorageJS
+
+	// The prefix appended to each key name. This is so that all keys created by
+	// this structure can be deleted without affecting other keys in external
+	// storage.
+	prefix string
+}
+
+// jsStorage is the global that stores Javascript as window.havenStorage.
+var jsExternalStorage ExternalStorage = newExternalStorage(externalStorageWasmPrefix)
+
+// newExternalStorage creates a new externalStorage object with the specified prefix.
+func newExternalStorage(prefix string) *externalStorage {
+	return &externalStorage{
+		v:      &HavenStorageJS{js.Global().Get("havenStorage")},
+		prefix: prefix,
+	}
+}
+
+// GetExternalStorage returns Javascript's external storage.
+func GetExternalStorage() ExternalStorage {
+	return jsExternalStorage
+}
+
+// Get decodes and returns the value from the external storage given its key
+// name. Returns os.ErrNotExist if the key does not exist.
+func (ls *externalStorage) Get(keyName string) ([]byte, error) {
+	value, err := ls.v.GetItem(ls.prefix + keyName)
+	if err != nil {
+		return nil, err
+	}
+
+	return base32768.SafeEncoding.DecodeString(value)
+}
+
+// Set encodes the bytes to a string and adds them to external storage at the
+// given key name. Returns an error if external storage quota has been reached.
+func (ls *externalStorage) Set(keyName string, keyValue []byte) error {
+	encoded := base32768.SafeEncoding.EncodeToString(keyValue)
+	return ls.v.SetItem(ls.prefix+keyName, encoded)
+}
+
+// RemoveItem removes a key's value from external storage given its name. If there
+// is no item with the given key, this function does nothing.
+func (ls *externalStorage) RemoveItem(keyName string) error {
+	return ls.v.RemoveItem(ls.prefix + keyName)
+}
+
+// Clear clears all the keys in storage. Returns the number of keys cleared and any error.
+func (ls *externalStorage) Clear() (int, error) {
+	// Get a copy of all key names at once
+	keys, err := ls.v.KeysPrefix(ls.prefix)
+	if err != nil {
+		return 0, err
+	}
+
+	// Loop through each key
+	for _, keyName := range keys {
+		if err := ls.RemoveItem(keyName); err != nil {
+			return 0, err
+		}
+	}
+
+	return len(keys), nil
+}
+
+// ClearPrefix clears all keys with the given prefix. Returns the number of
+// keys cleared and any error.
+func (ls *externalStorage) ClearPrefix(prefix string) (int, error) {
+	// Get a copy of all key names at once
+	keys, err := ls.v.KeysPrefix(ls.prefix + prefix)
+	if err != nil {
+		return 0, err
+	}
+
+	// Loop through each key
+	for _, keyName := range keys {
+		if err := ls.RemoveItem(prefix + keyName); err != nil {
+			return 0, err
+		}
+	}
+
+	return len(keys), nil
+}
+
+// Key returns the name of the nth key in externalStorage. Return [os.ErrNotExist]
+// if the key does not exist. The order of keys is not defined.
+func (ls *externalStorage) Key(n int) (string, error) {
+	keyName, err := ls.v.Key(n)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimPrefix(keyName, ls.prefix), nil
+}
+
+// Keys returns a list of all key names in external storage.
+func (ls *externalStorage) Keys() ([]string, error) {
+	keys, err := ls.v.KeysPrefix(ls.prefix)
+	if err != nil {
+		return nil, err
+	}
+	return keys, nil
+}
+
+// Length returns the number of keys in externalStorage.
+func (ls *externalStorage) Length() (int, error) {
+	length, err := ls.v.Length()
+	if err != nil {
+		return 0, err
+	}
+	return length, nil
+}
+
+// ExternalStorageUNSAFE returns the underlying external storage wrapper. This can be
+// UNSAFE and should only be used if you know what you are doing.
+//
+// The returned wrapper wraps all the functions and fields on the Javascript
+// havenStorage object to handle type conversions and errors. But it does not
+// decode/sanitize the inputs/outputs or track entries using the prefix system.
+// If using it, make sure all key names and values can be converted to valid
+// UCS-2 strings.
+func (ls *externalStorage) ExternalStorageUNSAFE() *HavenStorageJS {
+	return ls.v
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Javascript Wrappers                                                        //
+////////////////////////////////////////////////////////////////////////////////
+
+// HavenStorageJS stores the Javascript window.havenStorage object and wraps all
+// of its methods and fields to handle type conversations and errors.
+type HavenStorageJS struct {
+	js.Value
+}
+
+// GetItem returns the value from the external storage given its key name. Returns
+// [os.ErrNotExist] if the key does not exist.
+//
+// Doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
+func (ls *HavenStorageJS) GetItem(keyName string) (keyValue string, err error) {
+	defer exception.Catch(&err)
+	promise := ls.Call("getItem", keyName)
+	result, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return "", js.Error{Value: jsErr[0]}
+	}
+	if result[0].IsNull() {
+		return "", os.ErrNotExist
+	}
+	return result[0].String(), nil
+}
+
+// SetItem adds the value to external storage at the given key name. Returns an
+// error if external storage quota has been reached.
+//
+// Doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+func (ls *HavenStorageJS) SetItem(keyName, keyValue string) (err error) {
+	defer exception.Catch(&err)
+	promise := ls.Call("setItem", keyName, keyValue)
+	_, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return js.Error{Value: jsErr[0]}
+	}
+	return nil
+}
+
+// RemoveItem removes a key's value from external storage given its name. If there
+// is no item with the given key, this function does nothing.
+//
+// Doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage/removeItem
+func (ls *HavenStorageJS) RemoveItem(keyName string) error {
+	promise := ls.Call("removeItem", keyName)
+	_, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return js.Error{Value: jsErr[0]}
+	}
+	return nil
+}
+
+// Clear clears all the keys in storage.
+//
+// Doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage/clear
+func (ls *HavenStorageJS) Clear() error {
+	promise := ls.Call("clear")
+	_, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return js.Error{Value: jsErr[0]}
+	}
+	return nil
+}
+
+// Key returns the name of the nth key in externalStorage. Return [os.ErrNotExist]
+// if the key does not exist. The order of keys is not defined.
+//
+// Doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage/key
+func (ls *HavenStorageJS) Key(n int) (keyName string, err error) {
+	defer exception.Catch(&err)
+	promise := ls.Call("key", n)
+	result, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return "", js.Error{Value: jsErr[0]}
+	}
+	if result[0].IsNull() {
+		return "", os.ErrNotExist
+	}
+	return result[0].String(), nil
+}
+
+// Keys returns a list of all key names in external storage.
+func (ls *HavenStorageJS) Keys() ([]string, error) {
+	promise := ls.Call("keys")
+	result, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return []string{}, js.Error{Value: jsErr[0]}
+	}
+
+	keysJS := result[0]
+	keys := make([]string, keysJS.Length())
+	for i := range keys {
+		keys[i] = keysJS.Index(i).String()
+	}
+	return keys, nil
+}
+
+// KeysPrefix returns a list of all key names in external storage with the given
+// prefix and trims the prefix from each key name.
+func (ls *HavenStorageJS) KeysPrefix(prefix string) ([]string, error) {
+	promise := ls.Call("keys")
+	result, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return []string{}, js.Error{Value: jsErr[0]}
+	}
+
+	keysJS := result[0]
+	keys := make([]string, 0, keysJS.Length())
+	for i := 0; i < keysJS.Length(); i++ {
+		keyName := keysJS.Index(i).String()
+		if strings.HasPrefix(keyName, prefix) {
+			keys = append(keys, strings.TrimPrefix(keyName, prefix))
+		}
+	}
+	return keys, nil
+}
+
+// Length returns the number of keys in externalStorage.
+//
+// Doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage/length
+func (ls *HavenStorageJS) Length() (int, error) {
+	promise := ls.Call("length")
+	result, jsErr := utils.Await(promise)
+	if jsErr != nil {
+		return 0, js.Error{Value: jsErr[0]}
+	}
+	return result[0].Int(), nil
+}
